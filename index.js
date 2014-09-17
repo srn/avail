56b30c8b5e914fb9e956eaecbb5217cdc9bcf59c
Søren Rasmussen <brokzftw@gmail.com>
2014-09-17 21:00:25 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var request = require('request');
+var symbols = require('log-symbols');
+
+module.exports = function(domain, callback){
+  var path = 'https://domai.nr/api/json/search?q='+domain+'&client_id=avail';
+
+  request(path, function (error, response, body) {
+    var results = JSON.parse(body);
+    results = results.results;
+
+    results = results.map(function (domain) {
+      var available;
+
+      switch (domain.availability) {
+        case 'tld':
+          available = symbols.info;
+          break;
+        case 'unavailable':
+          available = symbols.error;
+          break;
+        case 'taken':
+          available = symbols.error;
+          break;
+        case 'available':
+          available = symbols.success;
+          break;
+      }
+
+      return domain.domain + domain.path + ' ' + available;
+    });
+
+    callback(results);
+  });
+}
