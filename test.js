a232bac9c8cf11b8a99fc8d09b93a23a1386953d
Søren Brokær <root@srn.io>
2014-09-18 13:23:15 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,27 +4,25 @@ var test = require('ava');
 var nock = require('nock');
 var avail = require('./');
 
+var fixtures = [
+  {
+    domain: 'avail.io',
+    availability: 'available'
+  },
+  {
+    domain: 'avail.com',
+    availability: 'unavailable'
+  }
+];
+
 nock('https://domai.nr')
   .get('/api/json/search?q=avail&client_id=avail')
-  .reply(200,
-    [
-      {
-        domain: 'avail.io',
-        availability: 'available'
-      },
-      {
-        domain: 'avail.com',
-        availability: 'unavailable'
-      }
-    ]
-  );
+  .reply(200, fixtures);
 
 test(function (t) {
   avail('avail', function (domains) {
-    domain = domains[0];
-
-    t.assert(domains[0], {domain: 'avail.io', availability: 'available'});
-    t.assert(domains[1], {domain: 'avail.com', availability: 'unavailable'});
+    t.assert(domains[0], fixtures[0]);
+    t.assert(domains[1], fixtures[1]);
 
     t.end();
   });
